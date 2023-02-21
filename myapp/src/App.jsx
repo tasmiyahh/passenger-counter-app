d6180092263bc0bc90439e6318969171c6b6759e
tasmiyahh <tasmiyahhussainn99@gmail.com>
2023-02-21 06:58:37 -0800

    new

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -11,7 +11,11 @@ function App() {
   }
 
   function save(){
+    let countstr = count + " - "
+    document.getElementById("save-el").innerHTML += countstr //jb hu chahy pehly ka bh rhy tw +use
     console.log(count)
+    document.getElementById("count-el").innerHTML = 0 //save kbad 0 hojaye
+    count = 0; //phir se inc kren tw 0 se start ho
   }
   return (
     <div className="App">
@@ -19,6 +23,7 @@ function App() {
     <h2 id="count-el">0</h2>
     <button id="increment-btn" onClick={()=>increment()}>Increment</button>
     <button id='save-btn' onClick={()=>save()}>Save</button>
+    <p id='save-el'>Previous entries: </p>
     </div>
   );
 }
